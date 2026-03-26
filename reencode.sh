adb17465f3a00d9fe28d9422a15fffa9c00a20c3
AnshpreetSingh351 <gujralanshpreetsingh@gmail.com>
2026-03-26 11:22:14 +0530

    x

--- reencode.sh
+++ reencode.sh
@@ -3,7 +3,7 @@ mkdir -p videos/optimized
 
 for i in 1 2 3 4 5 6; do
     echo "Encoding section${i}.mp4 ..."
-    ffmpeg -i "videos/section${i}.mp4" -c:v libx264 -g 1 -bf 0 -crf 20 -preset fast -an "videos/optimized/section${i}.mp4"
+    ffmpeg -i "videos/section${i}.mp4" -c:v libx264 -g 1 -bf 0 -crf 18 -preset fast -an "videos/optimized/section${i}.mp4"
 done
 
 echo "Done! Replace your old videos with the ones in videos/optimized/"
